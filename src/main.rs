38036b7810ad75dcd9f30a5ae2c560ac9fcea809
Paul Healey <code@healeys.dev>
2023-07-23 19:07:04 +0100

    Starting a todo file of top level gubbins

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,6 @@ mod db;
 mod paths;
 mod runner;
 
-/*
-    TODO: Save last run path to speed up app launch. Either tag on apps table or create new table.
-    TODO: Add method to update last run path periodically (or after each run?).
-    TODO: Add Tokio tracing for logging.
-    TODO: Improve console output. Make consistent. Make tidy. Show app name!
-    TODO: Change name to something less naff? Stream Deck Lemonator?
-    TODO: Stats/Usage table. What has been run and when.
-    TODO: Add "shortcut" option. Run app with no search term. May need to tweak input arguments.
-    TODO: Add a reset option. Nukes the DB and recreates it.
-    TODO: Add params field to apps table. Allows for passing of params to exe.
-    TODO: Make cross platform. MacOS and Windows. Rework path code with PathBuf.
-    TODO: Add an admin drive search option
-    TODO: Handle WhatsApp and it's weird folder "C:\Program Files\WindowsApps\5319275A.WhatsAppDesktop_2.2327.6.0_x64__cv1g1gvanyjgm\WhatsApp.exe"
-*/
-
 #[tokio::main]
 async fn main() {
     let args = cli::Args::parse();
